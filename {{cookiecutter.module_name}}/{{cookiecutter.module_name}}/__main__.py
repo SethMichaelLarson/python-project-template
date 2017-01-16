bbd2267e6e207c2106c5ebf347ace42917994e6c
Seth M. Larson <SethMichaelLarson@users.noreply.github.com>
2017-01-16 13:38:51 -0600

    Create __main__.py

---
+++ {{cookiecutter.module_name}}/{{cookiecutter.module_name}}/__main__.py
@@ -0,0 +1,6 @@
+""" This is the entry point for if your module is
+executed like this: ``python -m {{cookiecutter.module_name}}``. """
+from .cli import main
+
+if __name__ == '__main__':
+    main()
